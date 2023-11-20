b88bc2cc026154a1581ed359ae0a278db785b0f4
gero-boehm <gero_boehm@gmx.de>
2023-11-21 00:21:52 +0100

    modified minimap

--- src/utils/minimap/minimap_draw.c
+++ src/utils/minimap/minimap_draw.c
@@ -30,21 +30,25 @@ void	minimap_draw(t_scene *scene)
 	uint32_t	ix;
 	uint32_t	iy;
 	t_vec		position;
+	t_vec		middle;
 
 	ix = 0;
+	middle = vec_mult_scalar(scene->map_size, MAP_SCALE / 2);
+	draw_square(scene->minimap, vec_create(0, 0, 0), 10 * MAP_SCALE, 0x000000ff);
 	while (ix < scene->map_size.y)
 	{
 		iy = 0;
 		while (iy < scene->map_size.y)
 		{
+			// position = vec_sub(vec_create(ix, iy, 0), scene->player.position);
 			position = vec_create(ix, iy, 0);
 			minimap_draw_tile(scene, position);
 			iy++;
 		}
 		ix++;
 	}
+	// draw_square(scene->minimap, vec_sub(middle, vec_create(3, 3, 0)), 7, 0xff7800ff);
 	draw_square(scene->minimap, vec_sub(vec_mult_scalar(scene->player.position, MAP_SCALE), vec_create(3, 3, 0)), 7, 0xff7800ff);
-	draw_line(scene->minimap, vec_mult_scalar(scene->player.position, MAP_SCALE), vec_mult_scalar(vec_add(scene->player.position, player_get_look_at(&scene->player, 0)), MAP_SCALE), 0x00ff00ff);
 }
 
 void	minimap_draw_ray(t_scene *scene, t_vec vec)
@@ -52,6 +56,7 @@ void	minimap_draw_ray(t_scene *scene, t_vec vec)
 	t_vec	start;
 	t_vec	end;
 
+	// start = vec_mult_scalar(scene->map_size, MAP_SCALE / 2);
 	start = vec_mult_scalar(scene->player.position, MAP_SCALE);
 	end = vec_add(start, vec_mult_scalar(vec, MAP_SCALE));
 	draw_line(scene->minimap, start, end, 0xff0000ff);
